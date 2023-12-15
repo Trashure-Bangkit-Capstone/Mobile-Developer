c598023e86dfc3613d6622cfa1b18309b0b08ed4
Ngatour Rohman <ngatourrohman25@gmail.com>
2023-12-15 16:41:36 +0700

    Initial commit

---
+++ app/src/main/java/com/arthur/trashure/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.arthur.trashure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
