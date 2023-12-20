bf36862cf64b26b3258b3bd2f6b0e002b21cad18
Arthur <142135411+NgatourRohman@users.noreply.github.com>
2023-12-21 00:16:48 +0700

    Add files via upload

---
+++ app/src/main/java/com/capstone/trashure/data/local/User.kt
@@ -0,0 +1,5 @@
+package com.capstone.trashure.data.local
+
+// src/main/java/com/example/yourpackage/User.kt
+data class User(val username: String, val email: String, val password: String)
+
---
+++ app/src/main/java/com/capstone/trashure/view/admin/AdminHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.trashure.view.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.trashure.R
+
+class AdminHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+    }
+}
